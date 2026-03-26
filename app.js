9415f7720b2da6da936c8b56d522625ee822effb
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-03-26 22:08:43 +0530

    vercel

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ connectDatabase();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://taskmanager-client-three.vercel.app"]
+
+
+
   })
 );
 
